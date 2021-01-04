cd20131dbe4306965e14cd26cf00f63f996de2ca
Myeongho Ham <myeongho.ham@readnumber.com>
2021-01-04 15:23:43 +0900

    fix : 버전업

--- android/src/main/kotlin/com/jukqaz/media_scanner/MediaScannerPlugin.kt
+++ android/src/main/kotlin/com/jukqaz/media_scanner/MediaScannerPlugin.kt
@@ -9,7 +9,56 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.PluginRegistry.Registrar
 
-public class MediaScannerPlugin(
+class MediaScannerPlugin : FlutterPlugin, MethodCallHandler {
+    /// The MethodChannel that will the communication between Flutter and native Android
+    ///
+    /// This local reference serves to register the plugin with the Flutter Engine and unregister it
+    /// when the Flutter Engine is detached from the Activity
+    private lateinit var channel: MethodChannel
+    private lateinit var context: Context
+
+    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "media_scanner")
+        channel.setMethodCallHandler(this)
+        context = flutterPluginBinding.applicationContext
+    }
+
+    override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+        if (call.method == "scan") {
+            val path: String? = call.argument("path")
+            result.success(refreshMedia(path))
+        } else {
+            result.notImplemented()
+        }
+    }
+
+    /// function to refresh media on Android Device
+    private fun refreshMedia(path: String?): String {
+        return try {
+            if (path == null)
+                throw NullPointerException()
+            val file = File(path)
+            if (android.os.Build.VERSION.SDK_INT < 29) {
+                context.sendBroadcast(Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, Uri.fromFile(file)))
+            } else {
+                MediaScannerConnection.scanFile(context, arrayOf(file.toString()),
+                    arrayOf(file.name), null)
+            }
+            Log.d("Media Scanner", "Success show image $path in Gallery")
+            "Success show image $path in Gallery"
+        } catch (e: Exception) {
+            Log.e("Media Scanner", e.toString())
+            e.toString()
+        }
+
+    }
+
+    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+        channel.setMethodCallHandler(null)
+    }
+}
+
+/*public class MediaScannerPlugin(
     private val context: Context
 ) : MethodCallHandler {
     companion object {
@@ -29,4 +78,4 @@ public class MediaScannerPlugin(
             result.notImplemented()
         }
     }
-}
+}*/
